3439e37d5732354182965878e727019d257f6c3e
Sergii Lovygin <sergey.lovygin@yahoo.com>
2022-04-24 23:42:37 +0300

    add all required stubs

---
+++ README.md
@@ -0,0 +1,5 @@
+# Skeleton
+
+A quick project skeleton for easily starting new Python Application projects.
+
+---
---
+++ setup.py
@@ -0,0 +1,11 @@
+"""
+    This module contains setup instructions for $project_name project
+    This simple script is needed to use
+        - pip install -e .
+        - add $project_name = {editable = true, path = "."} to packages/dev-packages
+"""
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    setup()
